2ed0f9fd8635ff996a1c2770f156cbb78f83fdfd
Mykyta-Petrov <mykyta.petrov@sigma.software>
2023-11-15 15:29:04 +0200

    Add location form

---
+++ jstest/src/components/LocationForm.js
@@ -0,0 +1,74 @@
+import React, { useMemo } from 'react'
+import * as Yup from 'yup';
+import { Field, Form, Formik, ErrorMessage } from 'formik';
+import { Country, State } from 'country-state-city';
+
+function LocationForm({ submitHandler }) {
+  const countries = useMemo(() => Country.getAllCountries(), []);
+
+  const schema = Yup.object().shape({
+    country: Yup.string().required(),
+    state: Yup.string().required()
+  });
+
+  const initialValues = {
+    country: '',
+    state: ''
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={(values) => submitHandler({
+          country: JSON.parse(values.country), state: JSON.parse(values.state) })}>
+        {(formik) => {
+          const { errors, touched, isValid, dirty, values } = formik;
+          return (
+            <div>
+              <Form>
+                <div>
+                  <Field id='country' as='select' name='country'>
+                    {!values.country && (<option value=''>Select country</option>)}
+                    {countries.map((country) => (
+                      <option key={country.name} value={JSON.stringify(country)}>
+                        {country.name}
+                      </option>
+                    ))}
+                  </Field>
+                  <ErrorMessage name='country' component='span'></ErrorMessage>
+                </div>
+                <div>
+                  <Field id='state' as='select' name='state'>
+                    {!values.state && (<option value=''>Select state</option>)}
+                    {values.country && State.getStatesOfCountry(JSON.parse(values.country).isoCode).map((state) => (
+                      <option key={state.name} value={JSON.stringify(state)}>
+                        {state.name}
+                      </option>
+                    ))}
+                  </Field>
+                  <ErrorMessage name='state' component='span'></ErrorMessage>
+                </div>
+                {/*<div>
+                  <Field id='city' as='select' name='city'>
+                    {!values.city && (<option value=''>Select city</option>)}
+                    {values.country && values.state && City.getCitiesOfState(JSON.parse(values.country).isoCode, JSON.parse(values.state).isoCode).map((city) => (
+                      <option key={city.name} value={JSON.stringify(city)}>
+                        {city.name}
+                      </option>
+                    ))}
+                  </Field>
+                  <ErrorMessage name='city' component='span'></ErrorMessage>
+                    </div>*/}
+                <button type='submit' disabled={!(dirty && isValid)}>Submit</button>
+              </Form>
+            </div>
+          );
+        }}
+      </Formik>
+    </div>
+  )
+}
+
+export default LocationForm
--- jstest/src/pages/Home.js
+++ jstest/src/pages/Home.js
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from 'react'
+import LocationForm from '../components/LocationForm';
 
 function Home() {
-  const country = 'Ukraine';
-  const city = 'Kharkiv';
-  const [currDate, setCurrDate] = useState(new Date());
+  const [country, setCountry] = useState(null);
+  const [state, setState] = useState(null);
+
+  const currDate = new Date();
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/${city},${country}/${currDate.getFullYear()}-${currDate.getMonth() + 1}-${currDate.getDate()}?key=${process.env.REACT_APP_API_KEY}`);
-        setWeatherData(await response.json());
+        if (country && state) {
+          const response = await fetch(`${process.env.REACT_APP_API_URL}/${country.name},${state.name}/${currDate.getFullYear()}-${currDate.getMonth() + 1}-${currDate.getDate()}?key=${process.env.REACT_APP_API_KEY}`);
+          setWeatherData(await response.json());
+        }
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [country, state]);
+
+  function submitHandler(values) {
+    setCountry(values.country);
+    setState(values.state);
+  }
 
   return (
     <div>
-      <h2>Weather forecast for {city}, {country}, {currDate.toDateString()}</h2>
+      <LocationForm submitHandler={submitHandler}/>
+      {country && state && <h2>Weather forecast for {country.name}, {state.name}, {currDate.toDateString()}</h2>}
       <div>{weatherData?.days[0]?.description}</div>
       <div>
         {weatherData?.days[0]?.hours?.map((hour) => (
